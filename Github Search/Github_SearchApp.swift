e08780be57f2b74eaea682c91f348268a760c95c
samir-sayyed <sayyedsamir11@gmail.com>
2024-08-03 13:14:28 +0530

    Initial Commit

---
+++ Github Search/Github_SearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  Github_SearchApp.swift
+//  Github Search
+//
+//  Created by Samir Sayyed on 03/08/24.
+//
+
+import SwiftUI
+
+@main
+struct Github_SearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
